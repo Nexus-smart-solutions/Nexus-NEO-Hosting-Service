5617e4351654d845e9ee9a881215a850bcbd74cd
Nexus Smart Solution <info@nexus-dxb.com>
2026-01-31 10:05:26 +0200

    Update variables.tf

--- environments/customers/template/backend/variables.tf
+++ environments/customers/template/backend/variables.tf
@@ -1,32 +1,25 @@
 variable "customer_domain" {
-  description = "The domain name provided by the customer"
-  type        = string
+  type = string
 }
 
 variable "customer_email" {
-  description = "The email address of the customer"
-  type        = string
+  type = string
 }
 
 variable "plan_tier" {
-  description = "The subscription tier (basic, premium, etc.)"
-  type        = string
+  type = string
 }
 
 variable "client_id" {
-  description = "The unique identifier for the client"
-  type        = string
+  type = string
 }
 
-
 variable "region" {
-  description = "Target AWS region"
-  type        = string
-  default     = "us-east-2"
+  type    = string
+  default = "us-east-2"
 }
 
 variable "organization_name" {
-  description = "Standard prefix for organizational resource naming"
-  type        = string
-  default     = "hosting-company"
+  type    = string
+  default = "hosting-company"
 }
