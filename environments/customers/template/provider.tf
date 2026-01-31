9163625296791ec4f6dd068d4327e3564c9436fe
Nexus Smart Solution <info@nexus-dxb.com>
2026-01-31 11:17:00 +0200

    Create provider.tf

---
+++ environments/customers/template/provider.tf
@@ -0,0 +1,24 @@
+# provider.tf
+
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+  
+  default_tags {
+    tags = {
+      ManagedBy   = "Terraform"
+      Environment = var.environment
+      Customer    = var.customer_domain
+    }
+  }
+}
