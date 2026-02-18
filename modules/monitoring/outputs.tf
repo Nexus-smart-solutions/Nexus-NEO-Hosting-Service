a48007fb86bba1cdf44313f66c8b9737335fb420
Nexus Smart Solution <info@nexus-dxb.com>
2026-02-18 12:26:42 +0200

    Update outputs.tf

--- modules/monitoring/outputs.tf
+++ modules/monitoring/outputs.tf
@@ -4,31 +4,25 @@
 
 output "sns_topic_arn" {
   description = "SNS topic ARN for alarms"
-  value       = var.sns_topic_arn != "" ? var.sns_topic_arn : (
-    var.sns_topic_arn == "" && var.alert_email != "" ?
-    aws_sns_topic.alerts[0].arn :
-    null
-  )
+  value       = var.sns_topic_arn != "" ? var.sns_topic_arn : (try(aws_sns_topic.alerts[0].arn, ""))
 }
 
 output "dashboard_name" {
   description = "CloudWatch dashboard name"
-  value       = var.create_dashboard ? aws_cloudwatch_dashboard.main[0].dashboard_name : null
+  value       = var.create_dashboard ? try(aws_cloudwatch_dashboard.main[0].dashboard_name, null) : null
 }
 
 output "dashboard_url" {
   description = "CloudWatch dashboard URL"
-  value = var.create_dashboard ? 
-    "https://console.aws.amazon.com/cloudwatch/home?region=${data.aws_region.current.name}#dashboards:name=${aws_cloudwatch_dashboard.main[0].dashboard_name}" 
-    : null
+  value       = var.create_dashboard ? "https://console.aws.amazon.com/cloudwatch/home?region=${data.aws_region.current.name}#dashboards:name=${try(aws_cloudwatch_dashboard.main[0].dashboard_name, "")}" : null
 }
 
 output "alarm_names" {
   description = "Names of created alarms"
   value = compact([
     aws_cloudwatch_metric_alarm.cpu_high.alarm_name,
-    var.enable_disk_alarm ? aws_cloudwatch_metric_alarm.disk_high[0].alarm_name : "",
-    var.enable_memory_alarm ? aws_cloudwatch_metric_alarm.memory_high[0].alarm_name : ""
+    var.enable_disk_alarm ? try(aws_cloudwatch_metric_alarm.disk_high[0].alarm_name, "") : "",
+    var.enable_memory_alarm ? try(aws_cloudwatch_metric_alarm.memory_high[0].alarm_name, "") : ""
   ])
 }
 
@@ -36,7 +30,7 @@ output "alarm_count" {
   description = "Number of active alarms"
   value = length(compact([
     aws_cloudwatch_metric_alarm.cpu_high.alarm_name,
-    var.enable_disk_alarm ? aws_cloudwatch_metric_alarm.disk_high[0].alarm_name : "",
-    var.enable_memory_alarm ? aws_cloudwatch_metric_alarm.memory_high[0].alarm_name : ""
+    var.enable_disk_alarm ? try(aws_cloudwatch_metric_alarm.disk_high[0].alarm_name, "") : "",
+    var.enable_memory_alarm ? try(aws_cloudwatch_metric_alarm.memory_high[0].alarm_name, "")
   ]))
 }
